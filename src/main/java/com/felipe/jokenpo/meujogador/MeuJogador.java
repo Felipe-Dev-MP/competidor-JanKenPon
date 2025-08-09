3cb0cb3ec353e47548dc54cd885c9b61e6266e95
Felipe Madureira <felipepmadureira@gmail.com>
2025-08-09 14:33:48 -0300

    tentando criar uma estrategia mais robusta

--- src/main/java/com/felipe/jokenpo/meujogador/MeuJogador.java
+++ src/main/java/com/felipe/jokenpo/meujogador/MeuJogador.java
@@ -1,14 +1,60 @@
 package com.felipe.jokenpo.meujogador;
+
 import io.github.guisso.jankenpon.AbstractPlayer;
 import io.github.guisso.jankenpon.Move;
 
-public class MeuJogador 
-        extends AbstractPlayer{
+public class MeuJogador
+        extends AbstractPlayer {
 
-  private Move minhaUltimaJogada = null;
-  
-  private int contagemGeralPedra = 0;
-  private int contagemGeralPapel = 0;
-  private int contagemGeralTesoura = 0;
-  
+    @Override
+    public String getDeveloperName() {
+        return "Felipe Madureira";
+    }
+
+    private int contagemGeralPedra = 0;
+    private int contagemGeralPapel = 0;
+    private int contagemGeralTesoura = 0;
+    
+    private Move encontrarMovimentoMaisFrequente() {
+        if (contagemGeralPedra >= contagemGeralPapel && contagemGeralPedra >= contagemGeralTesoura) {
+            return Move.ROCK;
+        } else if (contagemGeralPapel >= contagemGeralPedra && contagemGeralPapel >= contagemGeralTesoura) {
+            return Move.PAPER;
+        } else {
+            return Move.SCISSORS;
+        }
+    }
+    
+    @Override
+    public Move makeMyMove(Move opponentPreviousMove) {
+
+        if (opponentPreviousMove == Move.NONE) {
+            return Move.ROCK;
+        }
+        
+        switch (opponentPreviousMove) {
+            case ROCK:
+                contagemGeralPedra++;
+                break;
+            case PAPER:
+                contagemGeralPapel++;
+                break;
+            case SCISSORS:
+                contagemGeralTesoura++;
+                break;
+        }
+
+        Move movimentoMaisFrequente = encontrarMovimentoMaisFrequente();
+
+        switch (movimentoMaisFrequente) {
+            case ROCK:
+                return Move.PAPER;
+            case PAPER:
+                return Move.SCISSORS;
+            case SCISSORS:
+                return Move.ROCK;
+            default:
+                return Move.ROCK;
+        }
+    }
 }
