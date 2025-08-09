a3a3582e68ec15da67a8f5c8fe80e6400519746a
Felipe Madureira <felipepmadureira@gmail.com>
2025-08-09 18:11:57 -0300

    voltando novamente para a primeira tentativa, pelo visto a mais eficiente até agora

--- src/main/java/com/felipe/jokenpo/meujogador/MeuJogador.java
+++ src/main/java/com/felipe/jokenpo/meujogador/MeuJogador.java
@@ -2,7 +2,6 @@ package com.felipe.jokenpo.meujogador;
 
 import io.github.guisso.jankenpon.AbstractPlayer;
 import io.github.guisso.jankenpon.Move;
-import java.util.Random;
 
 public class MeuJogador
         extends AbstractPlayer{
@@ -13,7 +12,6 @@ public class MeuJogador
     }
 
     private Move minhaUltimaJogada = null;
-    private final Random random = new Random();
     
     private Move getJogadaVencedora(Move jogada){
         switch (jogada) {
@@ -42,19 +40,11 @@ public class MeuJogador
         Move minhaProximaJogada;
 
         if(euVenci){
-           if (random.nextInt(100) < 80){ 
-                Move contraAtaqueEsperado = getJogadaVencedora(minhaUltimaJogada);
-                minhaProximaJogada = getJogadaVencedora(contraAtaqueEsperado);
-            } else {
-                minhaProximaJogada = getJogadaVencedora(minhaUltimaJogada);
-            }
+            Move contraAtaqueEsperado = getJogadaVencedora(minhaUltimaJogada);
+            minhaProximaJogada = getJogadaVencedora(contraAtaqueEsperado);
 
         }else if(empate){
-            if (random.nextInt(100) < 75){
-                minhaProximaJogada = getJogadaVencedora(opponentPreviousMove);
-            }else{
-                minhaProximaJogada = opponentPreviousMove;
-            }
+            minhaProximaJogada = getJogadaVencedora(opponentPreviousMove);
 
         }else{
             minhaProximaJogada = getJogadaVencedora(opponentPreviousMove);
