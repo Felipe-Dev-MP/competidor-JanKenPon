136cec914c4291813bf4e0b92da865b763aea301
Felipe Madureira <felipepmadureira@gmail.com>
2025-08-09 10:58:14 -0300

    criando jogador

---
+++ src/main/java/com/felipe/jokenpo/meujogador/MeuJogador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.felipe.jokenpo.meujogador;
+
+/**
+ *
+ * @author felip
+ */
+public class MeuJogador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
